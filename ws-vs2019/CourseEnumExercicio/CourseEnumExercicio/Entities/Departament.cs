7d9510e45c5c7e2b8658f801f8d5f0e694e4d57d
Jeferson Lima <jefersonlimadesouza@hotmail.com.br>
2020-06-28 07:07:16 -0400

    ADD CourseEnumExercicio

---
+++ ws-vs2019/CourseEnumExercicio/CourseEnumExercicio/Entities/Departament.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CourseEnumExercicio.Entities
+{
+    class Departament
+    {
+        public String Name { get; set; }
+
+        public Departament()
+        {
+
+        }
+
+        public Departament(String name)
+        {
+            if (string.IsNullOrEmpty(name) || string.IsNullOrWhiteSpace(name))
+            {
+                Console.WriteLine("Nome invalido!");
+            }
+            else
+            {
+                name = name.ToUpper();
+                name = name.Trim();
+                name = name.Replace(',', ' ');
+                name = name.Replace('.', ' ');
+                Name = name;
+            }
+        }
+    }
+}
---
+++ ws-vs2019/CourseEnumExercicio/CourseEnumExercicio/Entities/Worker.cs
@@ -0,0 +1,62 @@
+﻿using CourseEnumExercicio.Entities.Enums;
+using System;
+using System.Collections.Generic;
+
+namespace CourseEnumExercicio.Entities
+{
+    class Worker
+    {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Departament Departament { get; set; }
+        public List<HourContract> Contracts { get; set; } = new List<HourContract>();
+
+        public Worker()
+        {
+
+        }
+
+        public Worker(string name, WorkerLevel level, double baseSalary, Departament departament)
+        {
+            if (string.IsNullOrEmpty(name) || string.IsNullOrWhiteSpace(name))
+            {
+                Console.WriteLine("Nome invalido!");
+            }
+            else
+            {
+                name = name.ToUpper();
+                name = name.Trim();
+                name = name.Replace(',', ' ');
+                name = name.Replace('.', ' ');
+                Name = name;
+                Level = level;
+                BaseSalary = baseSalary;
+                Departament = departament;
+            }
+            
+        }
+        public void AddContract(HourContract contract)
+        {
+            Contracts.Add(contract);
+        }
+
+        public void RemoveContract(HourContract contract)
+        {
+            Contracts.Remove(contract);
+        }
+
+        public double Income(int year, int month)
+        {
+            double sum = BaseSalary;
+            foreach(HourContract contract in Contracts)
+            {
+                if(contract.Date.Year == year && contract.Date.Month == month)
+                {
+                    sum += contract.TotalValue();
+                }
+            }
+            return sum;
+        }
+    }
+}
