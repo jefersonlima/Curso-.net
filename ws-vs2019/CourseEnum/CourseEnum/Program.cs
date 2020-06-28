61b0f8a3711e637f688034bbc13c6ac09c2ddd89
Jeferson Lima <jefersonlimadesouza@hotmail.com.br>
2020-06-27 23:24:01 -0400

    Aula 115 Enumeracoes

---
+++ ws-vs2019/CourseEnum/CourseEnum/Program.cs
@@ -0,0 +1,27 @@
+﻿using CourseEnum.Entities;
+using CourseEnum.Entities.Enums;
+using System;
+
+namespace CourseEnum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(os);
+            Console.WriteLine(txt);
+        }
+    }
+}
